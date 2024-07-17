a5a6a32a415766c6b276f6e60e5ec62b40dc8b9b
Faheem506pk <faheemiqbalm@gmail.com>
2024-07-17 23:14:48 +0500

    send

--- src/components/Graphs.jsx
+++ src/components/Graphs.jsx
@@ -1,71 +1,71 @@
-import React from 'react'
-import Highcharts from "highcharts";
-import HighchartsReact from "highcharts-react-official";
-import { useFirebaseData } from "../hooks/useFirebaseData";
+import React from 'react';
+import Highcharts from 'highcharts';
+import HighchartsReact from 'highcharts-react-official';
+import { useFirebaseData } from '../hooks/useFirebaseData';
+
 export default function Graphs() {
   const { temperature, humidity } = useFirebaseData();
 
-    const highchartsOptions = {
-        colors: ["#ffffff"],
-        chart: {
-          type: "area",
-          margin: [0, 0, 0, 0],
-          backgroundColor: "transparent"
-        },
-        title: {
-          text: ""
-        },
-        credits: {
-          enabled: false
-        },
-        exporting: {
-          enabled: false
-        },
-        xAxis: {
-          min: 0,
-          gridLineColor: "white",
-          gridLineWidth: 0,
-          lineColor: "white",
-          categories: ["Sat", "Sun", "Mon", "Tue", "Wed", "Thu", "Fri"],
-          labels: {
-            style: {
-              color: "white"
-            }
+  const highchartsOptions = {
+    colors: ['#ffffff'],
+    chart: {
+      type: 'area',
+      margin: [0, 0, 0, 0],
+      backgroundColor: 'transparent'
+    },
+    title: {
+      text: ''
+    },
+    credits: {
+      enabled: false
+    },
+    exporting: {
+      enabled: false
+    },
+    xAxis: {
+      min: 0,
+      gridLineColor: 'white',
+      gridLineWidth: 0,
+      lineColor: 'white',
+      categories: ['Sat', 'Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri'],
+      labels: {
+        style: {
+          color: 'white'
+        }
+      }
+    },
+    yAxis: {
+      gridLineWidth: 0,
+      title: {
+        text: false
+      },
+      labels: {
+        enabled: false
+      }
+    },
+    legend: {
+      enabled: false
+    },
+    plotOptions: {
+      area: {
+        dataLabels: {
+          enabled: true,
+          style: {
+            fontWeight: 'bold',
+            textOutline: 'none',
+            color: 'white'
           }
         },
-        yAxis: {
-          gridLineWidth: 0,
-          title: {
-            text: false
-          },
-          labels: {
-            enabled: false
-          }
-        },
-        legend: {
-          enabled: false
-        },
-        plotOptions: {
-          area: {
-            dataLabels: {
-              enabled: true,
-              style: {
-                fontWeight: "bold",
-                textOutline: "none",
-                color: "white"
-              }
-            },
-            enableMouseTracking: false
-          }
-        },
-        series: [
-          {
-            name: "Reggane",
-            data: [16.0, 18.2, 23.1, 27.9, 32.2, 36.4, 39.8]
-          }
-        ]
-      };
-
+        enableMouseTracking: false
+      }
+    },
+    series: [
+      {
+        name: 'Reggane',
+        data: [16.0, 18.2, 23.1, 27.9, 32.2, 36.4, 39.8]
+      }
+    ]
+  };
 
   return (
     <main className="main-wrapper">
@@ -73,77 +73,7 @@ export default function Graphs() {
         <div className="dashboard">
           <section className="content">
             <div className="row g-4">
-              <div className="col-4">
-                <div className="card ui-card p-3">
-                  <div className="card-body-2 p-0">
-                    <ul className="list-group hour-list">
-                      <li className="list-group-item d-flex justify-content-between align-items-center">
-                        Now
-                        <span className="deg">
-                          <i className="bi bi-brightness-high-fill"></i>{" "}
-                          32&deg;C
-                        </span>
-                      </li>
-                      <li className="list-group-item d-flex justify-content-between align-items-center">
-                        11 PM
-                        <span className="deg">
-                          <i className="bi bi-brightness-high-fill"></i>{" "}
-                          31&deg;C
-                        </span>
-                      </li>
-                      <li className="list-group-item d-flex justify-content-between align-items-center">
-                        12 PM
-                        <span className="deg">
-                          <i className="bi bi-brightness-high-fill"></i>{" "}
-                          30&deg;C
-                        </span>
-                      </li>
-                      <li className="list-group-item d-flex justify-content-between align-items-center">
-                        01 AM
-                        <span className="deg">
-                          <i className="bi bi-brightness-high-fill"></i>{" "}
-                          29&deg;C
-                        </span>
-                      </li>
-                      <li className="list-group-item d-flex justify-content-between align-items-center">
-                        02 AM
-                        <span className="deg">
-                          <i className="bi bi-brightness-high-fill"></i>{" "}
-                          28&deg;C
-                        </span>
-                      </li>
-                      <li className="list-group-item d-flex justify-content-between align-items-center">
-                        03 AM
-                        <span className="deg">
-                          <i className="bi bi-brightness-high-fill"></i>{" "}
-                          27&deg;C
-                        </span>
-                      </li>
-                      <li className="list-group-item d-flex justify-content-between align-items-center">
-                        04 AM
-                        <span className="deg">
-                          <i className="bi bi-brightness-high-fill"></i>{" "}
-                          26&deg;C
-                        </span>
-                      </li>
-                    </ul>
-                  </div>
-                </div>
-              </div>
-              <div className="col-8">
-                <div className="card ui-card h-100 overflow-hidden">
-                  <div
-                    className="card-body p-0"
-                    style={{ maxHeight: "280px", overflow: "hidden" }}
-                  >
-                    <HighchartsReact
-                      highcharts={Highcharts}
-                      options={highchartsOptions}
-                    />
-                  </div>
-                </div>
-              </div>
-              <div className="col-4">
+              <div className="col-lg-4 col-md-6 col-sm-12">
                 <div className="card ui-card h-100">
                   <div className="card-body">
                     <h5 className="card-title">Humidity</h5>
@@ -160,7 +90,7 @@ export default function Graphs() {
                   </div>
                 </div>
               </div>
-              <div className="col-4">
+              <div className="col-lg-4 col-md-6 col-sm-12">
                 <div className="card ui-card h-100">
                   <div className="card-body">
                     <h5 className="card-title">Temperature</h5>
@@ -177,7 +107,7 @@ export default function Graphs() {
                   </div>
                 </div>
               </div>
-              <div className="col-4">
+              <div className="col-lg-4 col-md-6 col-sm-12">
                 <div className="card ui-card h-100">
                   <div className="card-body">
                     <h5 className="card-title">Dew Point</h5>
@@ -192,10 +122,73 @@ export default function Graphs() {
                   </div>
                 </div>
               </div>
+              <div className="col-lg-4 col-md-6 col-sm-12">
+                <div className="card ui-card p-3">
+                  <div className="card-body-2 p-0">
+                    <ul className="list-group hour-list">
+                      <li className="list-group-item d-flex justify-content-between align-items-center">
+                        Now
+                        <span className="deg">
+                          <i className="bi bi-brightness-high-fill"></i> 32&deg;C
+                        </span>
+                      </li>
+                      <li className="list-group-item d-flex justify-content-between align-items-center">
+                        11 PM
+                        <span className="deg">
+                          <i className="bi bi-brightness-high-fill"></i> 31&deg;C
+                        </span>
+                      </li>
+                      <li className="list-group-item d-flex justify-content-between align-items-center">
+                        12 PM
+                        <span className="deg">
+                          <i className="bi bi-brightness-high-fill"></i> 30&deg;C
+                        </span>
+                      </li>
+                      <li className="list-group-item d-flex justify-content-between align-items-center">
+                        01 AM
+                        <span className="deg">
+                          <i className="bi bi-brightness-high-fill"></i> 29&deg;C
+                        </span>
+                      </li>
+                      <li className="list-group-item d-flex justify-content-between align-items-center">
+                        02 AM
+                        <span className="deg">
+                          <i className="bi bi-brightness-high-fill"></i> 28&deg;C
+                        </span>
+                      </li>
+                      <li className="list-group-item d-flex justify-content-between align-items-center">
+                        03 AM
+                        <span className="deg">
+                          <i className="bi bi-brightness-high-fill"></i> 27&deg;C
+                        </span>
+                      </li>
+                      <li className="list-group-item d-flex justify-content-between align-items-center">
+                        04 AM
+                        <span className="deg">
+                          <i className="bi bi-brightness-high-fill"></i> 26&deg;C
+                        </span>
+                      </li>
+                    </ul>
+                  </div>
+                </div>
+              </div>
+              <div className="col-lg-8 col-md-12 col-sm-12">
+                <div className="card ui-card h-100 overflow-hidden">
+                  <div
+                    className="card-body p-0"
+                    style={{ maxHeight: '280px', overflow: 'hidden' }}
+                  >
+                    <HighchartsReact
+                      highcharts={Highcharts}
+                      options={highchartsOptions}
+                    />
+                  </div>
+                </div>
+              </div>
             </div>
           </section>
         </div>
       </div>
     </main>
-  )
+  );
 }
