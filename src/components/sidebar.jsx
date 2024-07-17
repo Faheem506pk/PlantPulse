3968e62e62c9334e2658f01fa7cb19de8ea1ac6d
Faheem506pk <faheemiqbalm@gmail.com>
2024-07-18 00:29:02 +0500

    send

--- src/components/sidebar.jsx
+++ src/components/sidebar.jsx
@@ -1,11 +1,20 @@
-import React from 'react';
-import { Link } from 'react-router-dom'; // Add this import
+import React, { useEffect } from 'react';
+import { Link } from 'react-router-dom';
 
 export default function Sidebar() {
+  useEffect(() => {
+    const sidebar = document.querySelector('.sidebar');
+    const navItems = document.querySelectorAll('.nav-item');
+    const itemHeight = 120; // Adjust this value based on the height of your nav items including padding and margins
+
+    const sidebarHeight = navItems.length * itemHeight;
+    sidebar.style.maxHeight = `${sidebarHeight}px`;
+  }, []);
+
   return (
     <div className="sidebar">
       <aside className="side-navbar">
-        <ul className="nav flex-column">
+        <ul className="nav ">
           <li className="nav-item">
             <Link to="/" className="nav-link active" aria-current="page">
               <span className="nav-icon">
