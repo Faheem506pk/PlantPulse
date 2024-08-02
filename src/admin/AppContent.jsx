4dcc33f0a02427f5b01c408c9d53d95b7ee7f15d
Faheem506pk <faheemiqbalm@gmail.com>
2024-08-02 12:37:54 +0500

    admin extend

--- src/admin/AppContent.jsx
+++ src/admin/AppContent.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React , { useState } from 'react'
 import { useFirebaseData } from "../hooks/useFirebaseData";
 import { toast, ToastContainer } from "react-toastify";
 
@@ -6,6 +6,15 @@ import { toast, ToastContainer } from "react-toastify";
 const AppContent = () => {
   const { temperature, tempUp, tempDown, humidity, humidUp, humidDown,
     ldr, moisture, moistureUp, moistureDown, servo, servoAngle, setData } = useFirebaseData();
+
+      // State to track which card is expanded
+  const [expandedCard, setExpandedCard] = useState(null);
+
+  // Toggle expanded state for a card
+  const handleCardClick = (cardType) => {
+    setExpandedCard(expandedCard === cardType ? null : cardType);
+  };
+
 
   return (
     <main >
@@ -15,57 +24,117 @@ const AppContent = () => {
           <section className="content">
             <div className="row g-4">
             <div className="col-lg-4 col-md-6 col-sm-12">
-                <div className="card admin-ui-card h-100">
+            <div
+                  className={`card admin-ui-card h-100 ${
+                    expandedCard === "temperature" ? "expanded" : ""
+                  }`}
+                  onClick={() => handleCardClick("temperature")}
+                >
                   <div className="card-body">
                     <h5 className="admin-card-title">Temperature</h5>
-                    <div className="d-flex justify-content-center ">
+                    <div className="d-flex justify-content-center">
                       <div className="weather-data admin-card-title">
                         <div className="weather-icon">
-                        <i class="bi bi-thermometer-half"></i>
+                          <i className="bi bi-thermometer-half"></i>
                         </div>
                         <h4 className="value" id="temperature">
                           {temperature} &deg;C
                         </h4>
+                        {expandedCard === "temperature" && (
+                          <div className="admin-temperature-details">
+                            <div className="admin-temperature-row gap-5">
+                              <div className="admin-temperature-label">
+                                <h5>Temperature Up:</h5>
+                                <p>{tempUp} &deg;C</p>
+                              </div>
+                              <div className="admin-temperature-label">
+                                <h5>Temperature Down:</h5>
+                                <p>{tempDown} &deg;C</p>
+                              </div>
+                            </div>
+                          </div>
+                        )}
+                      </div>
+                    </div>
+                  </div>
+                </div>
+                
+              </div>
+              <div className="col-lg-4 col-md-6 col-sm-12">
+                <div
+                  className={`card admin-ui-card h-100 ${
+                    expandedCard === "humidity" ? "expanded" : ""
+                  }`}
+                  onClick={() => handleCardClick("humidity")}
+                >
+                  <div className="card-body">
+                    <h5 className="admin-card-title">Humidity</h5>
+                    <div className="d-flex justify-content-center">
+                      <div className="weather-data admin-card-title">
+                        <div className="weather-icon">
+                          <i className="bi bi-droplet-half"></i>
+                        </div>
+                        <h4 className="value" id="humidity">
+                          {humidity}%
+                        </h4>
+                        {expandedCard === "humidity" && (
+                          <div className="admin-humidity-details">
+                            <div className="admin-humidity-row gap-5">
+                              <div className="admin-humidity-label">
+                                <h5>Humidity Up:</h5>
+                                <p>{humidUp}%</p>
+                              </div>
+                              <div className="admin-humidity-label">
+                                <h5>Humidity Down:</h5>
+                                <p>{humidDown}%</p>
+                              </div>
+                            </div>
+                          </div>
+                        )}
                       </div>
                     </div>
                   </div>
                 </div>
               </div>
-              <div className="col-lg-4 col-md-6 col-sm-12">
-                <div className="card admin-ui-card h-100">
+               {/* Soil Moisture Card */}
+               <div className="col-lg-4 col-md-6 col-sm-12">
+                <div
+                  className={`card admin-ui-card h-100 ${
+                    expandedCard === "moisture" ? "expanded" : ""
+                  }`}
+                  onClick={() => handleCardClick("moisture")}
+                >
                   <div className="card-body">
-                    <h5 className="admin-card-title">Humidity</h5>
+                    <h5 className="admin-card-title">Soil Moisture</h5>
                     <div className="d-flex justify-content-center">
                       <div className="weather-data admin-card-title">
-                        <div className="weather-icon ">
-                        <i className="bi bi-droplet-half"></i>
+                        <div className="weather-icon">
+                          <i className="bi bi-water"></i>
                         </div>
-                        <h4 className="value" id="humidity">
-                          {humidity}%
+                        <h4 className="value" id="moisture">
+                          {moisture}%
                         </h4>
+                        {expandedCard === "moisture" && (
+                          <div className="admin-moisture-details">
+                            <div className="admin-moisture-row gap-5">
+                              <div className="admin-moisture-label">
+                                <h5>Moisture Up:</h5>
+                                <p>{moistureUp}%</p>
+                              </div>
+                              <div className="admin-moisture-label">
+                                <h5>Moisture Down:</h5>
+                                <p>{moistureDown}%</p>
+                              </div>
+                            </div>
+                          </div>
+                        )}
                       </div>
                     </div>
                   </div>
                 </div>
               </div>
              
-              <div className="col-lg-4 col-md-6 col-sm-12">
-                <div className="card admin-ui-card h-100">
-                  <div className="card-body">
-                    <h5 className="admin-card-title">Soil Moisture</h5>
-                    <div className="d-flex justify-content-center">
-                      <div className="weather-data admin-card-title">
-                        <div className="weather-icon">
-                        <i className="bi bi-water"></i>
-                        </div>
-                        <h4 className="value" id="moisture">
-                          {moisture}%
-                        </h4>
-                      </div>
-                    </div>
-                  </div>
-                </div>
-              </div>
+              
               <div className="col-lg-4 col-md-6 col-sm-12">
               <div className="card admin-ui-card h-100">
                   <div className="card-body">
